c9b92555dfc12951b9900116eb81c1b320785bc0
nehabhopale <bnehabhopale2@gmail.com>
2021-11-12 11:18:11 +0530

    updated

--- day3/zeroValues/pass.go
+++ day3/zeroValues/pass.go
@@ -7,17 +7,20 @@ func main() {
 	fmt.Println(num) //10
 	fmt.Println(&num)
 	increment(num)
-	fmt.Println(num) //10 becaz increement num incremeneted its inner value
+	fmt.Println(num) //10 becaz increment num incremeneted its inner value
 	fmt.Println(&num)
 	refrenceIncrement(&num) //increment the above val
 	fmt.Println(num)
-	//var q *int
-	//fmt.Println(*q)
+	var q *int
+	//fmt.Println(*q) //error
+	fmt.Println(q)
 }
 func increment(num int) {
 	fmt.Println("inside increment func add of num", &num)
-	num = num + 20 //this increement only the value num whose scope is inside num only
+	num = num + 20 //this increement only the value num whose scope is inside increment only
 }
+
+//argument  is * int i.e. *dataType of variable who address u r sending
 func refrenceIncrement(num *int) { // num variable of type pointer pointing to int type eg.& num is pointer pointing to int type
 	fmt.Println("inside ref function value of passed num", num)
 	*num = *num + 30 //this is goint to increment the value present at location num
