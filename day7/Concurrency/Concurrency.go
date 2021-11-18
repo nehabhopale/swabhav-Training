3eadbdc14e4349539063807d889074117e2c2065
nehabhopale <bnehabhopale2@gmail.com>
2021-11-18 12:58:08 +0530

    concurrency task

---
+++ day7/Concurrency/Concurrency.go
@@ -0,0 +1,32 @@
+package main
+import ("fmt"
+"time"
+"sync"
+)
+var wg=sync.WaitGroup{}
+func main(){
+	fmt.Println("start of main")
+	wg.Add(2)
+	go delayedIteration1()
+	go delayedIteration2()
+	fmt.Println("end of main")
+}
+func delayedIteration1(){
+	for i:=0;i<3;i++{
+		time.Sleep(time.Second)
+		fmt.Println(i)
+	}
+	wg.Done()
+
+}
+func delayedIteration2(){
+	for i:=0;i<3;i++{
+		time.Sleep(time.Second)
+		fmt.Println(i)
+	}
+	hi()
+	wg.Done()
+}
+func hi(){
+	fmt.Println("hiiii neha")
+}
