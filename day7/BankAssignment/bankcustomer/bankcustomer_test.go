89548c37538711a2a393ab88462a2ad688296f76
nehabhopale <bnehabhopale2@gmail.com>
2021-11-18 11:59:57 +0530

    updated

--- day7/BankAssignment/bankcustomer/bankcustomer_test.go
+++ day7/BankAssignment/bankcustomer/bankcustomer_test.go
@@ -1,12 +1,20 @@
 package bankcustomer
-import "testing"
+import ("testing"
+"fmt")
 var accForTestsender *customer=New(1,"neha",334,5000,"savings",12)
 var accForTestreciver *customer=New(2,"pooja",384,2000,"savings",12)
 func TestMoneyTranfer(t *testing.T){
-	expected:=nil
-
-	actual:=accForTestsender.MoneyTransfer(accForTestreciver, 334, 384, 12, 100, "savings")
-	if expected!=actual{
-		t.Errorf("expected %v,actual %v",expected,actual)
+	
+	err:=accForTestsender.MoneyTransfer(accForTestreciver, 334, 384, 12, 1000, "savings")
+	if err!=nil{
+		fmt.Println("there is an error ")
+	}else{
+		expected:=uint64(4000)
+		t1,_:=accForTestsender.GetAccFromNo(334)
+		actual:=t1.GetBalance()
+		if expected!=actual{
+			t.Errorf("expected %v,actual %v",expected,actual)
+		}
 	}
+	
 }
