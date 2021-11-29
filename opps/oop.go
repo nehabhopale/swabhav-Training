10cf6bf2f68e9c01082293310dbf80891bebeb82
nehabhopale <bnehabhopale2@gmail.com>
2021-11-29 12:44:06 +0530

    update

--- opps/oop.go
+++ opps/oop.go
@@ -1,9 +1,14 @@
+package main
+ 
+import (
+    "fmt"
+)
 //inheritance by structure embedding
 type A struct{
-	name string
+	name string			//here name is encapsulated in A struct 
 }
 type B struct{
-	id string  // here i is small so we can't export it somewhere else directly
+	id string  // here i is small kept to achieve the abstraction
 	A
 }
 func (b B)details(){
@@ -15,5 +20,50 @@ func main(){
 	A:=A{"neha"}
 	B:=B{"1"}
 	B.details()
+	rectangle := Rectangle{
+     
+        length: 5.0,
+        width: 2.5,
+    }
+     
+
+    square := Square{
+     
+        side: 1.0,
+    }
+     
+    
+    fmt.Println( rectangle.Area())
+    fmt.Println(square.Area())
 }
-//encapsulation by keeping cap letters if we are exporting from somewhere and if we want to keep it private the small letters
+
+//polymorphism by interfaces
+//polymorphism is having many forms
+
+ //using figure interface we can execute area method on diff forms like rec sq etc
+type Figure interface{
+ 
+    Area() float64
+}
+ 
+type Rectangle struct{
+     
+    length float64
+    width float64
+}
+type Square struct{
+     
+   
+    side float64
+}
+
+func (rect Rectangle) Area() float64{
+ 
+    area := rect.length * rect.width
+    return area
+}
+func (sq Square) Area() float64{
+	 
+    area := sq.side * sq.side
+    return area
+}
