4b603fa86b0b9c410380279b7c08b95d9be22aa8
nehabhopale <bnehabhopale2@gmail.com>
2021-12-01 12:28:11 +0530

    updated with vending machine

--- design patterns/test/state/hasitem.go
+++ design patterns/test/state/hasitem.go
@@ -10,15 +10,18 @@ type HasItemState struct {
 
 //we r having item and we r requesting it
 func (i *HasItemState) RequestItem() error {
-	if i.vendingMachine.itemCount == 0 {
-		i.vendingMachine.SetState(i.vendingMachine.noItem)
-		return fmt.Errorf("No item present")
-	}
+	// if i.vendingMachine.itemCount == 0 {
+	// 	i.vendingMachine.SetState(i.vendingMachine.noItem)
+	// 	return fmt.Errorf("No item present")
+	// }
 	fmt.Println("Item requestd by customer")
 	i.vendingMachine.SetState(i.vendingMachine.itemRequested)
 	return nil
 }
 func (i *HasItemState) AddItem(count uint8) error {
+	if count == 0 {
+		return fmt.Errorf("count shouldn't be zero")
+	}
 	fmt.Println(count, "no of items added")
 	i.vendingMachine.IncrementItemCount(count)
 	return nil
